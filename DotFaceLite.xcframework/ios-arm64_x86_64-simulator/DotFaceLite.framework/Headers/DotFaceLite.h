7eace11513d4765e4745504570d926159e632517
skrdaz <113330444+skrdaz@users.noreply.github.com>
2023-02-13 18:21:51 +0700

    first

---
+++ DotFaceLite.xcframework/ios-arm64_x86_64-simulator/DotFaceLite.framework/Headers/DotFaceLite.h
@@ -0,0 +1,18 @@
+//
+//  DotFaceLite.h
+//  DotFaceLite
+//
+//  Created by Jakub Vallo on 04/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DotFaceLite.
+FOUNDATION_EXPORT double DotFaceLiteVersionNumber;
+
+//! Project version string for DotFaceLite.
+FOUNDATION_EXPORT const unsigned char DotFaceLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DotFaceLite/PublicHeader.h>
+
+
